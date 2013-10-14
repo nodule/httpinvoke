58957af8225fef48001ff494896621a9fe3a563a
Rob Halff <rob.halff@gmail.com>
2013-10-14 02:50:59 +0200

    simplify it first, no options

--- nodes/get/node.js
+++ nodes/get/node.js
@@ -1,3 +1,4 @@
 var obj = {};
 obj.httpinvoke = httpinvoke;
-output = [obj, 'httpinvoke', input.url, 'GET', input.options];
+//output = [obj, 'httpinvoke', input.url, 'GET', input.options];
+output = [obj, 'httpinvoke', input.url, 'GET'];
